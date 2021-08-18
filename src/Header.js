181e2777ec7a79bedbad41828e79127b1620a36c
Manikandan <82821281+Manikandan-t@users.noreply.github.com>
2021-08-18 14:58:49 +0530

    Add files via upload

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+
+function Header(){
+    let name="Manikandan";
+    return(
+        <div>
+            <h1>Hello Everyone</h1>
+            <h3>I'm {name}</h3>
+        </div>
+    )
+}
+
+export default Header;
